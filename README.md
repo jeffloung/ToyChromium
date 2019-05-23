5edbc96e3ccb7f8a233ab249d3c597bfd44339ac
jeff <jeffloung@gmail.com>
2019-05-23 23:47:09 +0800

    init
    初始化，并且完成一版

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # ToyChromium
-简单全屏浏览器
+### 说明
+
+本工具使用CefSharp.WinForms稳定版73.1.130插件，实现简单访问功能，去除Chrome中的各种友好提示。实现沉浸式访问
+
+需要最低framework4.5.2版本支持
+
+### 配置文件说明
+
+```ini
+[app]
+;是否全屏
+fullscreen=0
+;是否屏蔽右键
+disablemouseright=1
+;默认打开的url
+url=www.baidu.com
+;udp服务端监听端口号
+udpport=10010
+[command]
+;下面是命令列表，例，发送cmd1执行functiona()方法。如没有发送socket命令，则忽略
+cmd1=functiona()
+cmd2=functionb()
+```
---
+++ ToyChromium/MainFrm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using STLib;
+
+namespace ToyChromium
+{
+    public partial class MainFrm : Form
+    {
+        string configFile = "config.ini";
+        string fullscreen;
+        string mouseright;
+        Dictionary<string, string> commands;
+        Size mainSize;
+        string url="baidu.com";
+        ChromiumWebBrowser browser;
+
+        UdpServer udpServer;
+
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            string path = Environment.CurrentDirectory+"\\"+configFile;
+            InitConfig(path);
+
+            InitSrv();
+
+            
+        }
+
+        private void InitSrv()
+        {
+            udpServer = new UdpServer();
+            udpServer.NewRequestReceived += UdpServer_NewRequestReceived;
+            udpServer.Start(6810);
+        }
+
+        private void UdpServer_NewRequestReceived(System.Net.IPEndPoint ipe, string msg)
+        {
+            Console.WriteLine(msg);
+            if (commands.ContainsKey(msg))
+            {
+                Console.WriteLine("run:"+commands[msg]);
+                browser.ExecuteScriptAsync(commands[msg]);
+            }
+        }
+
+        private void InitConfig(string configPath)
+        {
+            url = IniHelper.ReadValue("app", "url", configPath);
+            fullscreen = IniHelper.ReadValue("app", "fullscreen", configPath, "0");
+            mouseright = IniHelper.ReadValue("app", "disablemouseright", configPath, "0");
+            commands = new Dictionary<string, string>();
+            commands = IniHelper.ReadKeyValues("command", configPath);
+            if (fullscreen == "1")
+            {
+                this.Location = new Point(0, 0);
+                mainSize.Height = Screen.PrimaryScreen.Bounds.Height;
+                mainSize.Width = Screen.PrimaryScreen.Bounds.Width;
+                this.Size = mainSize;
+            }
+
+            chPl.Size = this.Size;
+
+            browser = new ChromiumWebBrowser(url)
+            {
+                Dock = DockStyle.Fill
+            };
+            if (mouseright == "1")
+            {
+                browser.MenuHandler = new CustomMenuHandler();
+            }
+            chPl.Controls.Add(browser);
+            browser.Load(url);
+        }
+
+        private void MainFrm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (udpServer != null)
+            {
+                udpServer.Stop();
+            }
+        }
+    }
+}
